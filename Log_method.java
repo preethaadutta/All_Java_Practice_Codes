fc39ebbe2e74f781c1fb269bdc033259236b94a4
Preethaa Dutta <92708433+preethaadutta@users.noreply.github.com>
2023-05-28 21:50:51 +0530

    Update Log_method.java

--- Log_method.java
+++ Log_method.java
@@ -6,12 +6,12 @@ public class Main
 	public static void main(String[] args) 
 	{
 	    Scanner in=new Scanner(System.in);
-		System.out.println("Enter the number=");
+	    System.out.println("Enter the number=");
 	    int a=in.nextInt();
-		System.out.println("Enter the base=");
+	    System.out.println("Enter the base=");
 	    int b=in.nextInt();
-        double result1;
-        result1=Math.log(a)/Math.log(b);
-        System.out.println("Log of that number is="+result1);
+            double result1;
+            result1=Math.log(a)/Math.log(b);
+            System.out.println("Log of that number is="+result1);
 	}
 }
