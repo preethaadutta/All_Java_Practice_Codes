a9b3179a467d8673a10831b9f3f99bbe6035b5a4
Preethaa Dutta <92708433+preethaadutta@users.noreply.github.com>
2023-05-28 21:53:27 +0530

    Update Squareroot_without_method.java

--- Squareroot_without_method.java
+++ Squareroot_without_method.java
@@ -5,27 +5,27 @@ public class Main
 	public static void main(String[] args) 
 	{
 	    Scanner in=new Scanner(System.in);
-		System.out.println("Enter the number=");
+	    System.out.println("Enter the number=");
 	    int a=in.nextInt();
-		int squareroot=0;
-        for(int i=1;i<=a;i++)
-        {
-            if(a%i==0)
+	    int squareroot=0;
+            for(int i=1;i<=a;i++)
             {
-                if(i*i==a)
-                {
-                    squareroot=i;
-                }
-            }
-        }
-        if(squareroot==0)
-        {
-            squareroot=0;
-            System.out.println("Square root is="+squareroot);
-        }
-        else
-        {
-            System.out.println("Square root is="+squareroot);
-        }
+		    if(a%i==0)
+                    {
+			    if(i*i==a)
+                            {
+				    squareroot=i;
+                            }
+                     }
+             }
+             if(squareroot==0)
+	     {
+		     squareroot=0;
+                     System.out.println("Square root is="+squareroot);
+              }
+              else
+              {
+		      System.out.println("Square root is="+squareroot);
+              }
 	}
 }
